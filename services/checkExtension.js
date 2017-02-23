9d757728977bbe03042b072c2ced37706c7e98c4
biplabmalakar <biplab.x@venturepact.com>
2017-02-23 17:01:41 +0530

    .bmp extension solve

--- services/checkExtension.js
+++ services/checkExtension.js
@@ -1,6 +1,6 @@
 module.exports = (ext) => {
     console.log('In Check Extension Function...');
-    if (ext == ".jpg" || ext == '.png' || ext == '.jpeg' || ext == '.jifi' || ext == ".exif" || ext == '.tiff' || ext == '.gif' || ext == 'bmp' ||
+    if (ext == ".jpg" || ext == '.png' || ext == '.jpeg' || ext == '.jifi' || ext == ".exif" || ext == '.tiff' || ext == '.gif' || ext == '.bmp' ||
         ext == 'jpe' || ext == ".bpg" || ext == ".bat" || ext == ".heif" || ext == ".webp" || ext == ".ppm" || ext == ".pgm" || ext == ".pbm" || ext == ".pnm") {
         return 1;
     } else return 2;
