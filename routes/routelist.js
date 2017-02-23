820e81d3b38e6b60cef72b58c80fccf42531d4e8
biplabmalakar <biplab.x@venturepact.com>
2017-02-23 11:16:41 +0530

    cors added to cors-origin

--- routes/routelist.js
+++ routes/routelist.js
@@ -2,10 +2,12 @@ let express = require('express');
 let router = express.Router();
 let fs = require('fs');
 let bodyparser = require('body-parser');
+let cors = require('cors');
 let uploadController = require('../controller/uploadroute');
 let fetchFile = require('../controller/fetchFile');
 
 router.use(bodyparser());
+router.use(cors());
 
 router.post('/', uploadController.uploadFile)
 router.get('/:link', fetchFile.fetchFile)
