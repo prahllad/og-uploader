4db56256f910155a6700a78f1754bcd616f9ded9
Ajit <jainajit194@gmail.com>
2017-02-24 17:25:25 +0530

    Moduling done

---
+++ helper/linkLoader.js
@@ -0,0 +1,24 @@
+var path=require('path');
+var request=require('request');
+var fs=require('fs');
+module.exports=function(link){
+    return new Promise((resolve,reject)=>{
+            if(link){
+                var filename=path.parse(link);
+                var base64 = require('base-64');
+                var utf8 = require('utf8');
+                var bytes = utf8.encode(filename.name);
+                var encoded = base64.encode(bytes);
+                let writer;
+                request(link)
+                .pipe(writer=fs.createWriteStream('uploads/'+encoded+filename.ext));
+                writer.on("finish",()=>{
+                    resolve({path:'uploads/'+encoded+filename.ext})
+                });
+                
+            }
+            else{
+                resolve('none');
+            }
+    })
+}
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var path=require('path');
-var request=require('request');
 var bodyParser=require('body-parser');
 var fs=require('fs');
 var multiparty=require('connect-multiparty'),
@@ -12,7 +11,8 @@ AWS.config.update({
     secretAccessKey: secretAccessKey
 });
 var s3 = new AWS.S3();
-var compresser=require('./compresser');
+var compresser=require('./helper/compresser');
+var linkLoader=require('./helper/linkLoader');
 var express= require('express');
 var morgan = require('morgan');
 var hostname = 'localhost';
@@ -27,14 +27,17 @@ app.use(function(req, res, next) {
 });
 app.post('/',multipartyMiddleware,(req,res,next)=>{
         var file={};
-        if(req.body.link){
-            console.log(req.body.link);
-            var filename=path.basename(req.body.link);
-          let writer;
-            request(req.body.link)
-            .pipe(writer=fs.createWriteStream('uploads/'+filename));
-            writer.on("finish",()=>{
-                var compressResult=compresser('uploads/'+filename,'uploads/',req.body.quality);
+        var quality=req.body.quality;
+        var linkLoaderResult=linkLoader(req.body.link);
+        linkLoaderResult.then((obj)=>{
+            if(obj=='none'){
+                console.log(req.files.image,req.body.quality,req.files);
+                 file=req.files.image;
+            }
+            else{
+                file=obj;
+            }
+            var compressResult=compresser(file.path,'uploads/',quality);
                 compressResult.then((filepath)=>{
                     console.log(path.basename(filepath));
                     var stream = fs.createReadStream(filepath);
@@ -44,7 +47,7 @@ app.post('/',multipartyMiddleware,(req,res,next)=>{
                             console.log('error:'+err+"   data:",data);
                             if(!err){
                                 fs.unlink(filepath,(err)=>{
-                                    res.status(200).send({InkBlob:data});
+                                     res.status(200).send({InkBlob:data,status:1});
                                 });
                             }
                             else{
@@ -52,74 +55,15 @@ app.post('/',multipartyMiddleware,(req,res,next)=>{
                             }
                         })
                 }).catch((err)=>{
-                res.status(500).send('Internal Server :Unable to Compress the image');
+                     res.status(500).send('Internal Server :Unable to Compress the image');
                 })
-            });
-            
-        }
-        // console.log(req.files.image,req.body.quality,req.files);
-        // var file=req.files.image;
-        // var compressResult=compresser(file.path,'images/',req.body.quality);
-        // compressResult.then((filepath)=>{
-        //     console.log(path.basename(filepath));
-        //      var stream = fs.createReadStream(filepath);
-        //         var params = {Bucket: 'uploaddata123',Key:path.basename(filepath),Body:stream,ACL:"public-read",'Cache-Control':"max-age=1296000"};
-        //         var options = {partSize: 10 * 1024 * 1024, queueSize: 1};
-        //         s3.upload(params,options,(err, data)=>{
-        //             console.log('error:'+err+"   data:",data);
-        //             if(!err){
-        //                 fs.unlink(filepath,(err)=>{
-        //                     res.status(200).send({InkBlob:data});
-        //                 });
-        //             }
-        //             else{
-        //                  res.status(500).send("Unable to upload the file..");
-        //             }
-        //         })
-        // }).catch((err)=>{
-        //    res.status(500).send('Internal Server :Unable to Compress the image');
-        // })
-  
+
+        })
+        
 });
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
 });
-
 app.listen(port,hostname,function(){
 	console.log(`Server : http://${hostname}:${port}/`);
 });
-//  var stream = fs.createReadStream('big4.jpg');
-// var result=compresser(stream,'images/');
-// console.log(result);
-// result.then((file)=>{
-//     console.log('file'+file);
-//     fs.unlink(file);
-// });
-      
-    // s3.putObject(params, function (perr, pres) {
-    //   if (perr) {
-    //     console.log("Error uploading data: ", perr);
-    //   } else {
-    //                 var getParams = {
-    //             Bucket: 'uploaddata123', // your bucket name,
-    //             Key: file.originalFilename // path to the object you're looking for
-    //         }
-
-    //         s3.getObject(getParams, function(err, data) {
-    //             // Handle any error and exit
-    //             if (err)
-    //                 return err;
-
-    //         // No error happened
-    //         // Convert Body from a Buffer to a String
-
-    //                  // Use the encoding necessary
-    //                 console.log("Received Data:",data);
-    //                     s3.getSignedUrl('getObject', getParams, function(err, url){
-    //                     console.log('the url of the image is', url);
-                        
-    //                 res.end("ddhd");
-    //                 })
-    //         });
-    //   }
-    // });
