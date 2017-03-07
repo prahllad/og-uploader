a182300789f1b72e2bf388ceb98a500ce69f28cc
Himanshu Singh <himanshuakatalli@users.noreply.github.com>
2017-03-07 18:55:47 +0530

    asd

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 process.env['NODE_ENV']="Development";
 process.env['AWS_ACCESS_KEY']='';
 process.env['AWS_SECRET_KEY']='';
-process.env['BUCKET_NAME']='oguploader';
+process.env['BUCKET_NAME']='';
